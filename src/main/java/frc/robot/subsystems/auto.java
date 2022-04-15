f5d63b53c222a66d5d9144758fffc9a8962f8635
chargedpika <jhornak15@gmail.com>
2022-04-15 13:45:16 -0400

    Updated Fri 04/15/2022 at 13:45:16.39

--- src/main/java/frc/robot/subsystems/auto.java
+++ src/main/java/frc/robot/subsystems/auto.java
@@ -1,21 +1,23 @@
 package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+ 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
-import java.util.Collection;
-import java.util.List;
+ 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.Timer;
-import frc.robot.subsystems.mecanumOdometry;
+//import frc.robot.subsystems.mecanumOdometry;
 
 // Vision Imports
 import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.vision.VisionThread;
-import org.opencv.core.*;
-import frc.robot.visiontargets.BlueBallPipeline;
+// import edu.wpi.first.cameraserver.CameraServer;
+// import edu.wpi.first.vision.VisionThread;
+// import org.opencv.core.*;
+// import frc.robot.visiontargets.BlueBallPipeline;
+
+
 
 public class auto {
     // Drive elements
@@ -29,75 +31,30 @@ public class auto {
  
 
     // Vision Pipelines
-    private final Object imgLock = new Object();
-    private VisionThread visionThread; 
+    // private final Object imgLock = new Object();
+    // private VisionThread visionThread; 
    
 
-    private int videoWidth = 320;
-    private int videoHeight = 240;
+    // private int videoWidth = 320;
+    // private int videoHeight = 240;
 
-    private UsbCamera camera1;
+    // private UsbCamera camera1;
+
+
 
     // Auto
     double start;
     int step;
 
-    public auto(
-            shooterPID shooterDrive, 
-            autoCenter C, 
-            mecanumOdometry odo, 
-            TalonFX greenIntakeWheel, 
-            MecanumDrive mecanumDrive, 
-            UsbCamera cam1, 
-            String robotColor, 
-            solenoidCode S, 
-            talonSRXwheel intakeWheel
-            ) {
-        shooter = shooterDrive;
-        greenWheel = greenIntakeWheel;
-        mecDrive = mecanumDrive;
-        mecOdometry = odo;
-        center = C;
-        this.solenoid = S;
-        this.intake = intakeWheel;
+    interface IAutoPeriodic {
+        public void doit();
+    };
 
-        // camera1 = cam1;
-        // camera1.setResolution(videoWidth, videoHeight);
-        
-        // visionThread = new VisionThread(
-        //     camera1,
-        //     (robotColor == "blue") ? new BlueBallPipeline() : new BlueBallPipeline(), // change
-        //     pipeline -> {
-        //         if (!pipeline.findBlobsOutput().empty()) {
-        //             KeyPoint point = (pipeline.findBlobsOutput().toList().get(0));
-        //             Point pt = point.pt;
+    SendableChooser<IAutoPeriodic> autoChooser = new SendableChooser<>();
 
-        //             synchronized (imgLock) {
-        //                 currentXTarget = pt.x - (videoWidth/2);
-        //             }
-        //         }
-        //     }
-        // );
-        // visionThread.start();
-    }
-
-    public void autoStart() {
-        step = 0;
-        start = Timer.getFPGATimestamp();
-    }
-
-    private void nextStep() {
-        System.out.println ("nextStep: " + step);
-        step++;
-        start = Timer.getFPGATimestamp();
-        greenWheel.set(ControlMode.PercentOutput, 0.0);
-        shooter.setSpeed(0.0);
-        mecDrive.driveCartesian(0.0, 0.0, 0.0);
-    }
-
-    /*public void autoPeriodic() {
-        if (step == 0) {
-           
+    IAutoPeriodic auto2Ball = new IAutoPeriodic() {
+        public void doit() { 
+        if (step == 0) { 
             shooter.setSpeed(2200.0);
             greenWheel.set(ControlMode.PercentOutput, 0.5);
             if (Timer.getFPGATimestamp() - start >= 1) { nextStep(); }
@@ -141,22 +98,94 @@ public class auto {
             if (Timer.getFPGATimestamp() - start >= 3.0) { nextStep(); }
         }
     }
-*/
-public void autoPeriodic() {
-    if (step == 0) {
-        shooter.setSpeed(2200.0);
-       greenWheel.set(ControlMode.PercentOutput, 0.5);
-       if (Timer.getFPGATimestamp() - start >= 1) { nextStep(); }
-   } else if (step == 1) {
-        mecDrive.driveCartesian(0.35, 0.0, 0.0);
-        if (Timer.getFPGATimestamp() - start >= 3.0) { nextStep(); }
-    } else if (step == 2) {
-       // mecDrive.driveCartesian(0.0, 0.0, 0.35);
-        //if (Timer.getFPGATimestamp() - start >= 1.4) { nextStep(); }
-  //  } else if (step == 3) {
- //       mecDrive.driveCartesian(0.0, 0.0, (currentXTarget > 0) ? -0.05 : 0.05);
+    };  
+
+    IAutoPeriodic auto1Ball = new IAutoPeriodic() {
+        public void doit() { 
+            if (step == 0) {
+                shooter.setSpeed(2200.0);
+               greenWheel.set(ControlMode.PercentOutput, 0.5);
+               if (Timer.getFPGATimestamp() - start >= 1) { nextStep(); }
+           } else if (step == 1) {
+                mecDrive.driveCartesian(0.35, 0.0, 0.0);
+                if (Timer.getFPGATimestamp() - start >= 3.0) { nextStep(); }
+            } else if (step == 2) {
+               // mecDrive.driveCartesian(0.0, 0.0, 0.35);
+                //if (Timer.getFPGATimestamp() - start >= 1.4) { nextStep(); }
+            //  } else if (step == 3) {
+            //       mecDrive.driveCartesian(0.0, 0.0, (currentXTarget > 0) ? -0.05 : 0.05);
+            }
+        }
+    };
+    
+
+    public auto(
+            shooterPID shooterDrive, 
+            autoCenter C, 
+            mecanumOdometry odo, 
+            TalonFX greenIntakeWheel, 
+            MecanumDrive mecanumDrive, 
+            UsbCamera cam1, 
+            String robotColor, 
+            solenoidCode S, 
+            talonSRXwheel intakeWheel
+            ) {
+        shooter = shooterDrive;
+        greenWheel = greenIntakeWheel;
+        mecDrive = mecanumDrive;
+        mecOdometry = odo;
+        center = C;
+        this.solenoid = S;
+        this.intake = intakeWheel;
+
+
+        autoChooser.setDefaultOption("Great: 2 Ball", auto2Ball);
+        autoChooser.addOption("Lame:  1 Ball Auto", auto1Ball);
+
+       // Shuffleboard.selectTab("Drive");
+        SmartDashboard.putData(autoChooser);
+        // camera1 = cam1;
+        // camera1.setResolution(videoWidth, videoHeight);
+        
+        // visionThread = new VisionThread(
+        //     camera1,
+        //     (robotColor == "blue") ? new BlueBallPipeline() : new BlueBallPipeline(), // change
+        //     pipeline -> {
+        //         if (!pipeline.findBlobsOutput().empty()) {
+        //             KeyPoint point = (pipeline.findBlobsOutput().toList().get(0));
+        //             Point pt = point.pt;
+
+        //             synchronized (imgLock) {
+        //                 currentXTarget = pt.x - (videoWidth/2);
+        //             }
+        //         }
+        //     }
+        // );
+        // visionThread.start();
     }
-}
+
+    IAutoPeriodic selectedCommand;
+    
+    public void autoStart() {
+        selectedCommand = autoChooser.getSelected() ; 
+        step = 0;
+        start = Timer.getFPGATimestamp();
+        
+    }
+
+    private void nextStep() {
+        System.out.println ("nextStep: " + step);
+        step++;
+        start = Timer.getFPGATimestamp();
+        greenWheel.set(ControlMode.PercentOutput, 0.0);
+        shooter.setSpeed(0.0);
+        mecDrive.driveCartesian(0.0, 0.0, 0.0);
+    }
+
+    public void autoPeriodic() {
+        selectedCommand.doit(); 
+    }
+ 
     public void autoOdometry() {
         if (step == 0) {
             mecOdometry.updateSetpoints(3.0, 0.0, 0.0);
